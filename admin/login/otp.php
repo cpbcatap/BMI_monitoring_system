225ef62f154529445670bda8e9487cc1464cb191
cpbcatap <cpbcatap@gmail.com>
2026-02-13 20:15:08 +0800

    paste from bmi_monitoring

---
+++ admin/login/otp.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>BMI Monitoring System</title>
+  <link rel="stylesheet" href="../assets/css/icon.css">
+  <link rel="stylesheet" href="../assets/css/root.css">
+  <link rel="stylesheet" href="../assets/css/login.css">
+  <link rel="stylesheet" href="../assets/css/registration.css">
+   <link rel="stylesheet" href="../assets/css/otp.css">
+</head>
+
+<body>
+  <div class="main">
+    <div class="logo"><i class="uil--heart-rate"></i></div>
+    <div class="title">BMI Monitoring System</div>
+    <div class="subtitle">Know Your Numbers. Monitor Your Progress.</div>
+
+    <div class="form-container">
+
+        <div class="form-title">
+            Enter OTP:
+        </div>
+
+        <form class="otp-value">
+            <input name="otp" placeholder="#" required>
+            <input name="otp" placeholder="#" required>
+            <input name="otp" placeholder="#" required>
+            <input name="otp" placeholder="#" required>
+            <input name="otp" placeholder="#" required>
+            <input name="otp" placeholder="#" required>
+              
+        </form>
+        
+        <button type="submit" id="enter-otp">ENTER</button>
+
+        <div class="cancel-link">
+            <a href="register.php">Cancel</a>
+            </div>
+    </div>
+    
+
+  </div>
+</body>
+
+</html>
